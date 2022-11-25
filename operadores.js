4b4e02d916e2ec37a5ca9ed61bb9831b5ad820ae
Rociliu <mrrosa.carrasco@gmail.com>
2022-11-25 22:22:08 +0000

    primeros-ejercicios-js

---
+++ operadores.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 2;
+
+let suma = num1+num2;
+let modulo = num1%num2;
+let div = num1/num2;
+
+let text = "hola";
+let text2 = text + " mundo"
+;
---
+++ prueba.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log("Hola Mundo");
+
+var nombre;
+nombre = "Rocío";
+
+console.log(nombre);
+
+let apellido="Rosa";
+console.log(apellido)
+
+let num = 3;
+console.log(num);
+
+num=4;
+console.log(num);
+
+// Variables sin valor:
+let a;
+console.log(a); //me da en consola el valor undefined.
+
+a=null;
+console.log (a); // me da el valor null.
+
+console.log ("nombre", nombre, "apellido", apellido);
+
